cc6d9b29bf8ea989942c76f154c8e8102e451d25
Antrussian <antonino.russo.agent@gmail.com>
2023-12-21 02:53:35 +0100

    Creato componente genitore e figlio

---
+++ vite-comics-folder/src/components/ListaElements.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        
+        
+    </div>
+</template>
+
+
+<script>
+
+import ComicsElement from './ComicsElement.vue';
+export default {
+    
+}
+</script>
+
+
+
+<style lang="">
+    
+</style>
